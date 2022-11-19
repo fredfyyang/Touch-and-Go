676d38da6a6a12e4cb6724f33ba0094f64837cd8
fredfyyang <117394146+fredfyyang@users.noreply.github.com>
2022-11-19 03:18:17 -0500

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,1 +1,1 @@
-python train.py --dataroot /Path/to/train.txt --name touch_and_go --dataset_mode touch_and_go --gpu_ids 0,1,2,3 --batch_size 8 --model TDIS
+python train.py --dataroot /path/to/dataset --name touch_and_go --dataset_mode touch_and_go --gpu_ids 0,1,2,3 --batch_size 8 --model TDIS
