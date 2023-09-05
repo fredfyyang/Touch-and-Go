95e04d360f3bdad38c73cf2a790925098f008946
fredfyyang <117394146+fredfyyang@users.noreply.github.com>
2023-09-05 00:02:45 -0400

    Delete visuo-tactile-cmc directory

--- visuo-tactile-cmc/script/material_cmc_touch18.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python LinearProbing_touch.py --dataset touch_and_go --data_folder dataset/ --save_path ckpt/cls --model_path ckpt/cmc/memory_nce_16384_resnet18t2_lr_0.05_decay_0.0001_bsz_128_amount100_comment_view_Touch/ckpt_epoch_240.pth --model resnet18t2 --learning_rate 1.0 --layer 5
